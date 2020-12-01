e66406ae9b4a1941ca2b83226f5cd05115238273
TAREQ <mdtarequlislam634@gmail.com>
2020-12-01 12:07:32 +0600

    Dynamically changes recent home pages project

--- src/components/courses/Courses.js
+++ src/components/courses/Courses.js
@@ -2,72 +2,53 @@ import React, { Component, Fragment } from 'react';
 import { Container,Row,Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Img from '../../assets/images/blogpost.jpg';
+import AppUrl from '../../restApi/AppUrl';
+import RestClient from '../../restApi/RestClient';
 
 class courses extends Component {
+
+    constructor(){
+        super();
+        this.state = {
+            recentCourseData:[],
+        }
+    }
+    componentDidMount()
+    {
+        RestClient.GetRequest(AppUrl.course).then(result=>{
+            this.setState({recentCourseData:result});
+        })
+    }
     render() {
+        const courseArray = this.state.recentCourseData;
+        const recentCourseView = courseArray.map(result=>{
+            return <Col lg={6} md={12} sm={12} className="p-2">
+                        <Row>
+                            <Col lg={6} md={6} sm={12}>
+                                <img className="courseImage" src={result.small_image} />
+                            </Col>
+                            <Col lg={6} md={6} sm={12}>
+                                <h5 className="text-justify" >{result.sort_title}</h5>
+                                <p className="text-justify">
+                                {
+                                    result.sort_description
+                                }
+                                </p>
+                                
+                                <Link className="courseDetails float-left" to="/course-details">Details</Link>
+                            </Col>
+                        </Row>
+                    </Col>
+        });
         return (
             <Fragment>
                 <Container className="text-center">
                     <h2 className="serviceTitle">Our Courses</h2>
                     <Row>
-                        <Col lg={6} md={12} sm={12} className="p-2">
-                            <Row>
-                                <Col lg={6} md={6} sm={12}>
-                                    <img className="courseImage" src={Img} />
-                                </Col>
-                                <Col lg={6} md={6} sm={12}>
-                                    <h5 className="text-justify" >Web Development</h5>
-                                    <p className="text-justify">
-                                    Some quick example text to build on the card title and make up the bulk ofSome quick example text to build on the card title and make up the bulk ofSome quick example text to build on the card title and make 
-                                    </p>
-                                    
-                                    <Link className="courseDetails float-left" to="/course-details">Details</Link>
-                                </Col>
-                            </Row>
-                        </Col>
-                        <Col lg={6} md={12} sm={12} className="p-2">
-                            <Row>
-                                <Col lg={6} md={6} sm={12}>
-                                    <img className="courseImage" src={Img}/>
-                                </Col>
-                                <Col lg={6} md={6} sm={12}>
-                                <h5 className="text-justify">Web Development</h5>
-                                    <p className="text-justify">
-                                    Some quick example text to build on the card title and make up the bulk ofSome quick example text to build on the card title and make up the bulk ofSome quick example text to build on the card title and make 
-                                    </p>
-                                    <Link className="courseDetails float-left" to="/course-details">Details</Link>
-                                </Col>
-                            </Row>
-                        </Col>
-                        <Col lg={6} md={12} sm={12} className="p-2">
-                            <Row>
-                                <Col lg={6} md={6} sm={12}>
-                                    <img className="courseImage" src={Img}/>
-                                </Col>
-                                <Col lg={6} md={6} sm={12}>
-                                <h5 className="text-justify">Web Development</h5>
-                                    <p className="text-justify">
-                                    Some quick example text to build on the card title and make up the bulk ofSome quick example text to build on the card title and make up the bulk ofSome quick example text to build on the card title and make 
-                                    </p>
-                                    <Link className="courseDetails float-left" to="/course-details">Details</Link>
-                                </Col>
-                            </Row>
-                        </Col>
-                        <Col lg={6} md={12} sm={12} className="p-2">
-                            <Row>
-                                <Col lg={6} md={6} sm={12}>
-                                    <img className="courseImage" src={Img}/>
-                                </Col>
-                                <Col lg={6} md={6} sm={12}>
-                                <h5 className="text-justify">Web Development</h5>
-                                    <p className="text-justify">
-                                    Some quick example text to build on the card title and make up the bulk ofSome quick example text to build on the card title and make up the bulk ofSome quick example text to build on the card title and make 
-                                    </p>
-                                    <Link className="courseDetails float-left" to="/course-details">Details</Link>
-                                </Col>
-                            </Row>
-                        </Col>
-                    </Row>
+                        {
+                            recentCourseView
+                        }
+                    </Row> 
                 </Container>
             </Fragment>
         );
--- src/components/recentProject/RecentProject.js
+++ src/components/recentProject/RecentProject.js
@@ -47,45 +47,6 @@ class RecentProject extends Component {
                         {
                             projectView
                         }
-                        {/* <Col lg={4} md={6} sm={12}>
-                            <Card className="projectCard">
-                                <Card.Img variant="top" src={Img} />
-                                <Card.Body>
-                                    <Card.Title className="projectCardTitle">Card Title</Card.Title>
-                                    <Card.Text className="projectCardDescription">
-                                        Some quick example text to build on the card title and make up the bulk ofSome quick example text to build on the card title and make up the bulk ofSome quick example text to build on the card title and make up the bulk ofSome quick example text to build on the card title and make up the bulk ofSome quick example text to build on the card title and make up the bulk of
-                                    </Card.Text>
-                                    <Button variant="primary">Details</Button>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                        <Col lg={4} md={6} sm={12}>
-                            <Card className="projectCard">
-                                <Card.Img variant="top" src={Img} />
-                                <Card.Body>
-                                    <Card.Title className="projectCardTitle">Card Title</Card.Title>
-                                    <Card.Text className="projectCardDescription">
-                                    Some quick example text to build on the card title and make up the bulk ofSome quick example text to build on the card title and make up the bulk ofSome quick example text to build on the card title and make up the bulk ofSome quick example text to build on the card title and make up the bulk ofSome quick example text to build on the card title and make up the bulk of
-                                        
-                                    </Card.Text>
-                                    <Button variant="primary">Details</Button>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                        <Col lg={4} md={6} sm={12}>
-                            <Card className="projectCard">
-                                <Card.Img variant="top" src={Img} />
-                                <Card.Body>
-                                    <Card.Title className="projectCardTitle">Card Title</Card.Title>
-                                    <Card.Text className="projectCardDescription">
-                                    Some quick example text to build on the card title and make up the bulk ofSome quick example text to build on the card title and make up the bulk ofSome quick example text to build on the card title and make up the bulk ofSome quick example text to build on the card title and make up the bulk ofSome quick example text to build on the card title and make up the bulk of
-                                        
-                                    </Card.Text>
-                                    <Button variant="primary">Details</Button>
-                                </Card.Body>
-                            </Card>
-                        </Col> */}
-
                     </Row>
                 </Container>
             </Fragment>
