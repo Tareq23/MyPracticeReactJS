79f6b9e9913766ba4d94c337ed897ff481f134c7
TAREQ <mdtarequlislam634@gmail.com>
2020-12-02 01:15:31 +0600

    Dynamically fetch data on footer section

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,8 +4,27 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { Component, Fragment } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import AppUrl from '../../restApi/AppUrl';
+import RestClient from '../../restApi/RestClient';
 
 class Footer extends Component {
+    constructor()
+    {
+        super();
+        this.state = {
+            facebook: "",
+            youtube : "",
+            address : "",
+            email : "",
+            copyRight : "",
+        }
+    }
+    componentDidMount()
+    {
+        RestClient.GetRequest(AppUrl.footer).then(result=>{
+            this.setState({address:result[0]['address'],facebook:result[0]['facebook'],email:result[0]['email'],youtube:result[0]['youtube']});
+        });
+    }
     render() {
         return (
             <Fragment>
@@ -13,14 +32,18 @@ class Footer extends Component {
                     <Row>
                         <Col lg={3} md={6} sm={12} className="p-3 text-justify" >
                             <h2 className="serviceName">follow me</h2>
-                            <a className="socialLink" href="#"><FontAwesomeIcon icon={faFacebook}/> Facebook</a><br></br>
-                            <a className="socialLink" href="#"><FontAwesomeIcon icon={faYoutube}/>Youtube</a>
+                            <a className="socialLink" href={this.state.facebook}><FontAwesomeIcon icon={faFacebook}/> Facebook</a><br></br>
+                            <a className="socialLink" href={this.state.youtube}><FontAwesomeIcon icon={faYoutube}/>Youtube</a>
                         </Col>
                         <Col lg={3} md={6} sm={12} className="p-3 text-justify">
                             <h2 className="serviceName">address</h2>
-                            <p className="mb-0 serviceDescription">#79/6 Padma Residential Aria, 3rd Floor Front Side, Rajshahi</p>
-                            <p className="mt-0 serviceDescription"><FontAwesomeIcon icon={faEnvelope}/>tareq23@gmail.com</p>
-                            
+        <p className="mb-0 serviceDescription">{this.state.address}</p>
+                            <p className="mt-0 serviceDescription"><FontAwesomeIcon icon={faEnvelope}/>
+                                {
+                                    this.state.email
+                                }
+                            </p>
+                           
                         </Col>
                         <Col lg={3} md={6} sm={12} className="p-3 text-justify">
                             <h2 className="serviceName">Information</h2>
